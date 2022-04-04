eb21b04d75902e7d70bea20d1bd5805d0dab59f3
bubnovSA <bubnofwork@mail.ru>
2022-04-05 00:19:37 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+// основной модуль
+import gulp from 'gulp';
+// импорт модулей
+import { path } from './gulp/config/path.js';
+// Импорт общих плагинов
+import { plugins } from './gulp/config/plugins.js';
+
+
+// передача значений в глобальные переменные
+global.app = {
+    path: path,
+    gulp, gulp,
+    plugins: plugins
+}
+
+// Импорт задач
+import { copy } from './gulp/tasks/copy.js';
+import { reset } from './gulp/tasks/reset.js'
+import { html } from './gulp/tasks/html.js';
+
+// Наблюдатель за изменениями в файлах
+function watcher() {
+    gulp.watch(path.watch.files, copy);
+    gulp.watch(path.watch.html, html);
+}
+
+const mainTasks = gulp.parallel(copy, html);
+
+// Построение сценариев выполнения задач
+const dev = gulp.series(reset, mainTasks, watcher);
+
+//выполнение сценария по умолчанию
+gulp.task('default', dev);
